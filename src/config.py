1de2b225297ff48836c764ecc9e03090b267cf0c
immerse <thismailisdispensable@gmail.com>
2017-09-12 14:49:36 +0200

    make the system less centralized

--- src/config.py
+++ src/config.py
@@ -1,7 +1,1 @@
 
-
-initial_port = 5000
-initial_ip = "127.0.0.1"
-
-
-
--- src/main.py
+++ src/main.py
@@ -15,24 +15,24 @@ def receive_ping(node_id):
 def find_node():
 	return "TODO:node_id"
 
-
-def send_ping(port):
+def send_ping(ip, port):
 	my_id = "TODO_node_id"
 	data = {"port": port}
-	url = "http://%s:%d/api/kademlia/nodes/%s/" % (config.initial_ip,
-		config.initial_port, my_id)
+	url = "http://%s:%d/api/kademlia/nodes/%s/" % (ip, port, my_id)
 	req = requests.put(url, data=data)
+	print(req.text)
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		print("No port given")
 		exit(1)
 
-	port = int(sys.argv[1])
+	my_port = int(sys.argv[1])
 
-	if port != config.initial_port:
-		send_ping(port)
+	if len(sys.argv) > 3:
+		initial_peer_ip = sys.argv[2]
+		initial_peer_port = int(sys.argv[3])
+		send_ping(initial_peer_ip, initial_peer_port)
 
-	app.run(port=port)
-
+	app.run(port=my_port)
 
