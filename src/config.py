55e8c7d2bb3d9bd3a3e45f546fad69fa2d3a9e15
immerse <thismailisdispensable@gmail.com>
2017-09-12 14:35:39 +0200

    implement receiving of PINGs

---
+++ src/config.py
@@ -0,0 +1,7 @@
+
+
+initial_port = 5000
+initial_ip = "127.0.0.1"
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,20 +1,38 @@
 from flask import Flask
 import sys
+import config
+import requests
 
 # needed for Flask to work
 app = Flask(__name__)
 
 
-@app.route("/api/kademlia/ping", methods=['POST'])
-def receive_ping():
-    return "Hello,World!"
+@app.route("/api/kademlia/nodes/<node_id>/", methods=["PUT"])
+def receive_ping(node_id):
+	return "Hello, %s" % node_id
 
-
-@app.route("/api/kademlia/nodes", methods=['GET'])
+@app.route("/api/kademlia/nodes/", methods=["GET"])
 def find_node():
-    return "TODO:node_id"
+	return "TODO:node_id"
 
+
+def send_ping(port):
+	my_id = "TODO_node_id"
+	data = {"port": port}
+	url = "http://%s:%d/api/kademlia/nodes/%s/" % (config.initial_ip,
+		config.initial_port, my_id)
+	req = requests.put(url, data=data)
 
 if __name__ == "__main__":
-    port = int(sys.argv[1])
-    app.run(port=port)
+	if len(sys.argv) < 2:
+		print("No port given")
+		exit(1)
+
+	port = int(sys.argv[1])
+
+	if port != config.initial_port:
+		send_ping(port)
+
+	app.run(port=port)
+
+
