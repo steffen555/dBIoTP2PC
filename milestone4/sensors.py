dd4f04e5752d1163e454ab23c9c6384f70a6f27f
RPi <my@pi.com>
2017-10-04 17:12:12 +0000

    We are now going to Carl's Jr., and we don't want to lose our code.

--- milestone4/sensors.py
+++ milestone4/sensors.py
@@ -17,55 +17,64 @@ MOTION_PIN = 11
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def render_root():
-	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 12)
-	nearby = bool(GPIO.input(MOTION_PIN))
-	return render_template("index.html", temperature=temperature,
-										 humidity=humidity,
-										 nearby=nearby) 
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 12)
+    nearby = bool(GPIO.input(MOTION_PIN))
+    return render_template("index.html", temperature=temperature,
+                           humidity=humidity,
+                           nearby=nearby)
+
 
 @app.route("/pi/sensors/temperature/0/")
 def get_temperature():
-	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 12)
-	return json.dumps([time.time(), temperature])
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 12)
+    return json.dumps([time.time(), temperature])
+
 
 @app.route("/pi/sensors/humidity/0/")
 def get_humidity():
-	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 12)
-	return json.dumps([time.time(), humidity])
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 12)
+    return json.dumps([time.time(), humidity])
+
 
 @app.route("/pi/sensors/motion/0/")
 def get_motion():
-	return json.dumps([time.time(), GPIO.input(MOTION_PIN)])
+    return json.dumps([time.time(), GPIO.input(MOTION_PIN)])
+
 
 @app.route("/pi/actuators/leds/0/", methods=["POST"])
 def handle_led_set_value():
-	value = GPIO.LOW if request.form["value"] == "0" else GPIO.HIGH
-	GPIO.output(LED_PIN, value)
-	return "OK"
+    value = GPIO.LOW if request.form["value"] == "0" else GPIO.HIGH
+    GPIO.output(LED_PIN, value)
+    return "OK"
+
 
 def connect_sensor_to_kademlia(sensor_url, description):
-	sensor_id = id_generation.generate_id(sensor_url)
-	headers = {"node_id": str(sensor_id), "url": sensor_url, "description": description}
-	url = "http://%s:%d/api/wotds/registration/" % (INITIAL_PEER_IP, INITIAL_PEER_PORT)
-	requests.get(url, headers=headers)
+    sensor_id = id_generation.generate_id(sensor_url)
+    headers = {"node_id": str(sensor_id), "url": sensor_url, "description": description}
+    url = "http://%s:%d/api/wotds/registration/" % (INITIAL_PEER_IP, INITIAL_PEER_PORT)
+    requests.get(url, headers=headers)
+
 
 def connect_sensors_to_kademlia():
-	my_url = "http://%s:%d" % (WEB_SERVER_IP, WEB_SERVER_PORT)
-	connect_sensor_to_kademlia(my_url + "/pi/sensors/humidity/0/", "Humidity Sensor")
-	connect_sensor_to_kademlia(my_url + "/pi/sensors/temperature/0/", "Temperature Sensor")
-	connect_sensor_to_kademlia(my_url + "/pi/sensors/motion/0/", "Motion Sensor")
+    my_url = "http://%s:%d" % (WEB_SERVER_IP, WEB_SERVER_PORT)
+    connect_sensor_to_kademlia(my_url + "/pi/sensors/humidity/0/", "Humidity Sensor")
+    connect_sensor_to_kademlia(my_url + "/pi/sensors/temperature/0/", "Temperature Sensor")
+    connect_sensor_to_kademlia(my_url + "/pi/sensors/motion/0/", "Motion Sensor")
+
 
 def main():
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(MOTION_PIN, GPIO.IN)
-	GPIO.setup(LED_PIN, GPIO.OUT)
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(MOTION_PIN, GPIO.IN)
+    GPIO.setup(LED_PIN, GPIO.OUT)
 
-	connect_sensors_to_kademlia()
+    connect_sensors_to_kademlia()
 
-	app.run(host="0.0.0.0", port=WEB_SERVER_PORT)
+    app.run(host="0.0.0.0", port=WEB_SERVER_PORT)
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- milestone4/start_script.py
+++ milestone4/start_script.py
@@ -3,15 +3,15 @@ import random
 import time
 import os
 
-number_of_peers = 100
+number_of_peers = 7
 
 peers_list = []
-p = subprocess.Popen(["python2", "main.py", "5000"])
+p = subprocess.Popen(["python", "wotds.py", "5000"])
 for i in range(0, number_of_peers - 1):
     time.sleep(0.3)
-    p = subprocess.Popen(["python2", "main.py", "%d" % (5001 + i), "127.0.0.1", "%d" % (5000 + random.randint(0, i/5))])
+    p = subprocess.Popen(["python", "wotds.py", "%d" % (5001 + i), "127.0.0.1", "%d" % (5000 + random.randint(0, i/5))])
     peers_list.append(p)
 raw_input("Type anything to close all peers\n")
 p.kill()
-os.system("killall python2")
+os.system("killall python")
 print("Done!")
